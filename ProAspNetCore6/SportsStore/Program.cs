418966c5972c6e1900e413cbe95a1c5724adaaa5
ahumbawapa@gmx.net <ahumbawapa@gmx.net>
2023-05-17 15:07:35 +0200

    Configuring Entity Framework Core

--- ProAspNetCore6/SportsStore/Program.cs
+++ ProAspNetCore6/SportsStore/Program.cs
@@ -1,9 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using SportsStore.Models;
+
 //Das builder-Objekt wird verwendet, um Objekte (services) zu instanzieren
 //und per Dependency Injection bereit zu stellen
 var builder = WebApplication.CreateBuilder(args);
 
 //Stellt geteilte Objekte für das MVC Framework bereit
 builder.Services.AddControllersWithViews();
+
+//EFCore muss konfiguriert werden, so dass der Datenbank-Typ
+//und der ConnectionString sowie die Context-Klasse bekannt ist
+builder.Services.AddDbContext<StoreDbContext>(opts =>
+{
+    opts.UseSqlServer( // SqlServer wird verwendet
+        // Die IConfiguration-Schnittstelle bietet Zugriff auf das 
+        // AspNetCore - Konfigurationssystem in appsettings.json
+        // über das der ConnectionString ausgelesen werden kann
+        builder.Configuration["ConnectionStrings:SportsStoreConnection"]
+        );
+});
 
 var app = builder.Build();
 
