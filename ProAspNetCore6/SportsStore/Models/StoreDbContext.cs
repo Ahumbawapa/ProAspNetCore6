b203db4809c7b4efcf40c24bb6633b4f5249d3ae
ahumbawapa@gmx.net <ahumbawapa@gmx.net>
2023-05-17 15:00:17 +0200

    Creating the Database Context Class

---
+++ ProAspNetCore6/SportsStore/Models/StoreDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace SportsStore.Models
+{
+    // EFCore bietet Zugriff auf eine Datenbank durch eine Context-Klasse
+    // 
+    public class StoreDbContext : DbContext
+    {
+        public StoreDbContext(DbContextOptions<StoreDbContext> options)
+            :base(options)
+        {
+            
+        }
+
+        public DbSet<Product> Products => Set<Product>();
+    }
+}
