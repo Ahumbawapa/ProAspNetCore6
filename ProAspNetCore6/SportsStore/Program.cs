43806ac6362bcf86a0c7eea1864fe3c930e86791
ahumbawapa@gmx.net <ahumbawapa@gmx.net>
2023-05-17 14:14:41 +0200

    Preparing the Application Services and the Request Pipeline

--- ProAspNetCore6/SportsStore/Program.cs
+++ ProAspNetCore6/SportsStore/Program.cs
@@ -1,6 +1,17 @@
+//Das builder-Objekt wird verwendet, um Objekte (services) zu instanzieren
+//und per Dependency Injection bereit zu stellen
 var builder = WebApplication.CreateBuilder(args);
+
+//Stellt geteilte Objekte für das MVC Framework bereit
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//app.MapGet("/", () => "Hello World!");
+app.UseStaticFiles(); // stellt Unterstützung für statische Seiten in wwwroot bereit
+
+//Registriert das MVC - Framework als Quelle von Endpunkten mit
+//Standard-Mapping Einstellungen
+app.MapDefaultControllerRoute();
 
 app.Run();
