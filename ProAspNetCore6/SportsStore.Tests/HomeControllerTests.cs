cc7ccf14d6f2916e5f0f8c03ea81965e589b2829
ahumbawapa@gmx.net <ahumbawapa@gmx.net>
2023-05-17 19:59:57 +0200

    Adding Pagination - Schritt 02 Ergänzung Unit-Test für Pagination

--- ProAspNetCore6/SportsStore.Tests/HomeControllerTests.cs
+++ ProAspNetCore6/SportsStore.Tests/HomeControllerTests.cs
@@ -36,4 +36,37 @@ public class HomeControllerTests
 
 
     }
+    
+    [Fact]
+    public void Can_Paginate()
+    {
+        //Adding Pagination - Schritt 02
+
+        //Arrange
+        Mock<IStoreRepository> mock = new Mock<IStoreRepository>();
+        mock.Setup(m => m.Products).Returns((
+            new Product[]
+            {
+                new Product{ ProductID = 1, Name = "P1"},
+                new Product{ ProductID = 2, Name = "P2"},
+                new Product{ ProductID = 3, Name = "P3"},
+                new Product{ ProductID = 4, Name = "P4"},
+                new Product{ ProductID = 5, Name = "P5"}
+            }).AsQueryable<Product>());
+
+        HomeController controller = new HomeController(mock.Object);
+        controller.PageSize = 3;
+
+        //Act
+        IEnumerable<Product>? result =
+            //(controller.Index(2) as ViewResult)?.ViewData.Model 
+            //    as IEnumerable<Product> ?? Enumerable.Empty<Product>();
+            (controller.Index(2) as ViewResult).Model as IEnumerable<Product>;
+
+        // Assert
+        Product[] prodArray = result?.ToArray() ?? Array.Empty<Product>();
+        Assert.True(prodArray.Length == 2);
+        Assert.Equal("P4", prodArray[0].Name);
+        Assert.Equal("P5", prodArray[1].Name);
+    }
 }
