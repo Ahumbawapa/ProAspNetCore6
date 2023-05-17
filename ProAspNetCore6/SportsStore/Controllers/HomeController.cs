2ed2e839b3b94c88010b9d29570de1df6bd70068
ahumbawapa@gmx.net <ahumbawapa@gmx.net>
2023-05-17 19:40:19 +0200

    Adding Pagination - Schritt 01 Erweitern der Index - Methode

--- ProAspNetCore6/SportsStore/Controllers/HomeController.cs
+++ ProAspNetCore6/SportsStore/Controllers/HomeController.cs
@@ -6,12 +6,23 @@ namespace SportsStore.Controllers
     public class HomeController : Controller
     {
         private IStoreRepository _repository;
+        public int PageSize = 4; //Anzahl der Produkte pro Seite
 
         public HomeController(IStoreRepository repository)
         {
             _repository = repository;
         }
 
-        public IActionResult Index() => View(_repository.Products);
+        public IActionResult Index(int productPage = 1)
+        {
+            IQueryable<Product> products =
+                _repository.Products
+                .OrderBy(p => p.ProductID)
+                .Skip((productPage - 1) * PageSize)
+                .Take(PageSize);
+            
+                
+                return View(products); 
+        }
     }
 }
