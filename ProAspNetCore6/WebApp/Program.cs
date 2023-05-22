bb99b7ca58d0f9f6e26f324c4b88ce140bf376c9
ahumbawapa@gmx.net <ahumbawapa@gmx.net>
2023-05-22 21:34:26 +0200

    Erstellen eines WebDienstes über elementare Route-Konfiguration (Middleware) in Program.cs

--- ProAspNetCore6/WebApp/Program.cs
+++ ProAspNetCore6/WebApp/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using WebApp.Models;
+using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +12,54 @@ builder.Services.AddDbContext<DataContext>(opts =>
 
 
 var app = builder.Build();
+
+const string BASEURL = "api/products";
+app.MapGet($"{BASEURL}/{{id}}"
+    , async (HttpContext context, DataContext data) =>
+    {
+        string? id = context.Request.RouteValues["id"] as string;
+        if(null != id)
+        {
+            Product? p = data.Products.Find(long.Parse(id));
+            
+            if (null == p)
+            {
+                context.Response.StatusCode = StatusCodes.Status404NotFound;
+            }
+            else
+            {
+                context.Response.ContentType = "application/json";
+                await context.Response.WriteAsync(
+                    JsonSerializer.Serialize<Product>(p)
+                    ); 
+            }
+        }
+    });
+
+
+app.MapGet(BASEURL, async (HttpContext context, DataContext data) =>
+        {
+            context.Response.ContentType = "application/json";
+
+            await context.Response.WriteAsync(
+                JsonSerializer.Serialize<IEnumerable<Product>>(data.Products)
+                );
+        }
+
+    );
+
+app.MapPost(BASEURL, async (HttpContext context, DataContext data) =>
+    { 
+        Product? p = await JsonSerializer.DeserializeAsync<Product>(context.Request.Body);
+
+        if (null != p)
+        {
+            await data.AddAsync(p);
+            await data.SaveChangesAsync();
+            context.Response.StatusCode = StatusCodes.Status200OK;
+        }
+    }
+);
 
 app.UseMiddleware<WebApp.TestMiddleware>();
 
