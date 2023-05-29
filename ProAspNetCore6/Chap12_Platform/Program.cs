1167002fffa903177724cdfbbfa886b523561400
ahumbawapa@gmx.net <ahumbawapa@gmx.net>
2023-05-29 10:49:51 +0200

    Ergänzung Middleware - Komponente in Programm.cs

--- ProAspNetCore6/Chap12_Platform/Program.cs
+++ ProAspNetCore6/Chap12_Platform/Program.cs
@@ -1,6 +1,25 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+//Eigene Middleware wird durch app.Use eingebunden
+app.Use(async (context, next) =>
+{
+
+    if (context.Request.Method == HttpMethods.Get
+         && context.Request.Query["custom"] == "true")
+    {
+        // wird ausgeführt bei Aufruf http://localhost:5000/?custom=true
+
+        string message = $"Request.Header: {context.Request.Headers.ToString()}";
+        message += $"\nCustom middleware";
+
+
+        context.Response.ContentType = "text/plain";
+        await context.Response.WriteAsync(message);
+    }
+    await next(); // pass the request to the next component in the middleware pipeline
+});
+
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
