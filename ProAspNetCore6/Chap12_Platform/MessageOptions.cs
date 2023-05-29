03c6283430a0b0b6e72e682ee57a91e99bf8e4cf
ahumbawapa@gmx.net <ahumbawapa@gmx.net>
2023-05-29 14:33:07 +0200

    Configuring Middleware Using the options pattern in Program.cs middleware

---
+++ ProAspNetCore6/Chap12_Platform/MessageOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace Chap12_Platform
+{
+    public class MessageOptions
+    {
+        public string CityName { get; set; } = "New York";
+        public string CountryName { get; set; } = "USA";
+    }
+}
--- ProAspNetCore6/Chap12_Platform/Program.cs
+++ ProAspNetCore6/Chap12_Platform/Program.cs
@@ -1,64 +1,21 @@
+using Microsoft.Extensions.Options;
+using Chap12_Platform;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Verwendung des Options-Pattern
+builder.Services.Configure<MessageOptions>(options => {
+    options.CityName = "Albany";
+});
+
 var app = builder.Build();
 
-
-// Hier wird IApplicationBuilder.Map Methode verwendet, um einen
-// Middleware-Zweig zu starten
-((IApplicationBuilder)app).Map("/branch", branch => {
-
-    //branch.UseMiddleware<Chap12_Platform.QueryStringMiddleWare>();
-    //aufruf der terminierenden Variante
-    branch.Run(new Chap12_Platform.QueryStringMiddleWare().Invoke);
-    
-    // Terminal Middleware beendet die Request-Pipeline
-    // Die Asp.Net core Run() -> Methode ist ein bequemer Weg, Terminal-Middleware zu erzeugen
-    branch.Run(async (context) =>
-    {
-        await context.Response.WriteAsync($"Branch Middleware (terminated by Run()) \n");
-    });
-
-});
-
-// Anwort der Http-Anfrage anpassen
-app.Use(async (context, next) =>
+app.MapGet("/location", async (HttpContext context, IOptions<MessageOptions> msgOpts) =>
 {
-    await next(); // zur nächsten Middleware-Komponente weiterreichen
-    await context.Response.WriteAsync($"\nStatus Code: {context.Response.StatusCode}");
+    Chap12_Platform.MessageOptions opts = msgOpts.Value;
+    await context.Response.WriteAsync($"{opts.CityName}, {opts.CountryName}");
+
 });
 
-//Kurzschließen der Request Pipeline
-app.Use(async (context, next) =>
-{
-    if (context.Request.Path == "/short")
-    {
-        //ohne Aufruf von await next() wird hier direkt die Antwort gestartet
-        await context.Response.WriteAsync($"Request short circuited!");
-        
-    }
-    else
-    { 
-        await next();
-    }
-});
-
-
-//Eigene Middleware wird durch app.Use eingebunden
-app.Use(async (context, next) =>
-{
-
-    if (context.Request.Method == HttpMethods.Get
-         && context.Request.Query["custom"] == "true")
-    {
-        // wird ausgeführt bei Aufruf http://localhost:5000/?custom=true
-
-        context.Response.ContentType = "text/plain";
-        await context.Response.WriteAsync("Custom middleware \n");
-    }
-    await next(); // pass the request to the next component in the middleware pipeline
-});
-
-app.UseMiddleware<Chap12_Platform.QueryStringMiddleWare>();
-
-app.MapGet("/", () => "Hello World!");
-
+app.MapGet("/", () => "Hello, World!");
 app.Run();
