02f182b152590c6d8954a20776814eda2ef1149b
ahumbawapa@gmx.net <ahumbawapa@gmx.net>
2023-05-29 22:52:34 +0200

    Dealing with Related Data cylcle reference error!

--- ProAspNetCore6/WebApp/Controllers/SuppliersController.cs
+++ ProAspNetCore6/WebApp/Controllers/SuppliersController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using WebApp.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebApp.Controllers
 {
@@ -17,7 +18,9 @@ namespace WebApp.Controllers
         [HttpGet("{id}")]
         public async Task<Supplier?> GetSupplier(long id)
         {
-            return await context.Suppliers.FindAsync(id);
+            return await context.Suppliers
+                .Include(s => s.Products)
+                .FirstAsync(s => s.SupplierId == id);
         }
     }
 }
