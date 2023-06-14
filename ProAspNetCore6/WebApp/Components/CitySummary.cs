911c32b6a9269b080a6ae6f049692d962e435586
ahumbawapa@gmx.net <ahumbawapa@gmx.net>
2023-06-14 21:12:53 +0200

    24. Using View Components Preparing for This Chapter

---
+++ ProAspNetCore6/WebApp/Components/CitySummary.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Components
+{
+    public class CitySummary : ViewComponent
+    { 
+        private CitiesData _cityData;
+
+        public CitySummary(CitiesData cityData)
+        {
+            _cityData = cityData;
+        }
+
+        public string Invoke()
+        {
+            return $"{_cityData.Cities.Count()} cities, "
+                + $"{_cityData.Cities.Sum(c => c.Population)} people";
+        }
+    }
+}
