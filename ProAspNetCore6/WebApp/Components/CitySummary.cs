db1ebe240beaa030f8c84515bfd6bb62c74443fb
ahumbawapa@gmx.net <ahumbawapa@gmx.net>
2023-06-18 18:00:15 +0200

    Returning HTML Fragments

--- ProAspNetCore6/WebApp/Components/CitySummary.cs
+++ ProAspNetCore6/WebApp/Components/CitySummary.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
 using WebApp.Models;
 
 namespace WebApp.Components
@@ -12,14 +14,33 @@ namespace WebApp.Components
             _cityData = cityData;
         }
 
+        //public IViewComponentResult Invoke()
+        //{
+        //    CityViewModel cvm = new CityViewModel
+        //    {
+        //        Cities = _cityData.Cities.Count(),
+        //        Population = _cityData.Cities.Sum(c => c.Population)
+        //    };
+        //    return View(cvm);
+        //}
+
         public IViewComponentResult Invoke()
         {
-            CityViewModel cvm = new CityViewModel 
-            { 
-                Cities = _cityData.Cities.Count(),
-                Population = _cityData.Cities.Sum(c => c.Population)
-            };
-            return View(cvm);
+            // Razor-Ansicht zurückgeben /Views/Shared/Components/CitySummary/Default.cshtml
+            //    CityViewModel cvm = new CityViewModel
+            //    {
+            //        Cities = _cityData.Cities.Count(),
+            //        Population = _cityData.Cities.Sum(c => c.Population)
+            //    };
+            //    return View(cvm);
+
+            // Html- codierten String zurückgeben
+            //return Content("This is a <h3><i>string</i><h3>");
+
+            //Interpretierten HTML - String zurückgeben
+            return new HtmlContentViewComponentResult(new HtmlString("This is a <h3><i>string</i><h3>"));
+
         }
+
     }
 }
